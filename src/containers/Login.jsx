6d37c1de58310f09581728066ba53731f8a49909
Mahesh3000 <mahesh.sivngi@gmail.com>
2024-10-15 14:28:41 -0500

    second commit

---
+++ src/containers/Login.jsx
@@ -0,0 +1,13 @@
+import React, { memo } from "react";
+
+const Login = () => {
+  console.log("mahesh");
+
+  return (
+    <div>
+      <h1>Mahesh</h1>
+    </div>
+  );
+};
+
+export default Login;
