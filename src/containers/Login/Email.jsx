80c8853eea7da7a365f2809e7813bca683415cfd
Mahesh3000 <mahesh.sivngi@gmail.com>
2024-10-29 02:19:41 -0500

    email page changes

--- src/containers/Login/Email.jsx
+++ src/containers/Login/Email.jsx
@@ -1,11 +1,20 @@
 import React, { memo } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Email = () => {
   console.log("kjhksjh");
+  const navigate = useNavigate();
+
+  const handleBackClick = () => {
+    navigate("/");
+  };
 
   return (
     <div className="email-page">
       <div className="email-container">
+        <div className="back-button" onClick={handleBackClick}>
+          &#8592; {/* This is a left arrow character */}
+        </div>
         <h2>Login with Email</h2>
         <input
           type="email"
